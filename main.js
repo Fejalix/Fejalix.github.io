4b8fb372aea66299ff24bbdc9684b8637980fec2
felix <felix.janot@edu.devinci.fr>
2022-01-17 23:04:23 +0100

    :tada: finalisation du site + :bug: fix

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
   });
+
+ // window.onscroll = function () {
+
+ //   if (document.documentElement.scrollTop > 80) {
+   //   document.getElementById("navbar").style.background = "#31202B";
+  
+   // }
+   // else {
+     //   document.getElementById("navbar").style.background = "none";
+    //}
+
+
+//} 
